97a53c410e4df495599f681c56ed1828020737cb
sjoraa <16521528@mahasiswa.itb.ac.id>
2024-06-11 11:13:52 +0700

    feat: fix notification sidebar

--- apps/components/homescreen/Header.tsx
+++ apps/components/homescreen/Header.tsx
@@ -1,51 +1,18 @@
-import { Platform, StyleSheet, View, Text, SafeAreaView, TouchableOpacity, Animated, Dimensions, TouchableWithoutFeedback } from 'react-native';
-import React, { useRef, useState } from 'react';
+import { StyleSheet, View, Text, SafeAreaView, TouchableOpacity } from 'react-native';
+import React from 'react';
 import { verticalScale, moderateScale, horizontalScale } from '../../../themes/Metrics';
 import { Ionicons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
-import NotificationScreen from '../../screen/NotificationScreen';
 
-export default function Header() {
+interface HeaderProps {
+  openSidebar: () => void;
+}
+
+export default function Header({ openSidebar }: HeaderProps) {
   const navigation: any = useNavigation();
 
   const handleProfilePress = () => {
     navigation.navigate('Profile');
-  };
-
-  const sidebarWidth = useRef(new Animated.Value(0)).current;
-  const sidebarTranslateX = useRef(new Animated.Value(Dimensions.get('window').width)).current;
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-
-  const openSidebar = () => {
-    setIsSidebarOpen(true);
-    Animated.parallel([
-      Animated.timing(sidebarWidth, {
-        toValue: Dimensions.get('window').width * 0.75,
-        duration: 200,
-        useNativeDriver: false,
-      }),
-      Animated.timing(sidebarTranslateX, {
-        toValue: 0,
-        duration: 200,
-        useNativeDriver: false,
-      }),
-    ]).start();
-  };
-
-  const closeSidebar = () => {
-    setIsSidebarOpen(false);
-    Animated.parallel([
-      Animated.timing(sidebarWidth, {
-        toValue: 0,
-        duration: 200,
-        useNativeDriver: false,
-      }),
-      Animated.timing(sidebarTranslateX, {
-        toValue: Dimensions.get('window').width,
-        duration: 200,
-        useNativeDriver: false,
-      }),
-    ]).start();
   };
 
   return (
@@ -57,19 +24,6 @@ export default function Header() {
       <TouchableOpacity style={styles.iconContainer} onPress={openSidebar}>
         <Ionicons name="notifications" size={24} color="white" />
       </TouchableOpacity>
-
-      {isSidebarOpen && (
-        <>
-          <TouchableWithoutFeedback onPress={closeSidebar}>
-            <View style={styles.underlay} />
-          </TouchableWithoutFeedback>
-          <NotificationScreen
-            sidebarWidth={sidebarWidth}
-            sidebarTranslateX={sidebarTranslateX}
-            closeSidebar={closeSidebar}
-          />
-        </>
-      )}
     </SafeAreaView>
   );
 }
@@ -81,8 +35,8 @@ const styles = StyleSheet.create({
     height: verticalScale(90),
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'space-between', // Distribute items evenly along the main axis
-    paddingHorizontal: horizontalScale(10), // Add horizontal padding
+    justifyContent: 'space-between',
+    paddingHorizontal: horizontalScale(10),
     borderBottomLeftRadius: 20,
     borderBottomRightRadius: 20,
     zIndex: 10,
@@ -99,14 +53,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     margin: 20,
-  },
-  underlay: {
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0,
-    backgroundColor: 'rgba(0, 0, 0, 0.7)',
-    zIndex: 9,
   },
 });
